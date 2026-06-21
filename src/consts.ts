66ff75b60e0c5ec4eae5b063c768012919579169
shentao <38794455@qq.com>
2026-06-21 17:12:02 +0800

    chore: 改名啦 —— 开飞机的舒克 🚀

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '沈涛的博客';
+export const SITE_TITLE = '开飞机的舒克';
 export const SITE_DESCRIPTION = '资深后端工程师的技术分享与思考';
